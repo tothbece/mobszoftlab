8ba8f2c3ba6a3b6a79cfb1f92dcdb501f9f0f271
tothbece <tothbece@lauder.hu>
2022-04-20 15:35:03 +0200

    create skeleton files

--- app/src/main/java/hu/bme/aut/android/kimiquotes/ui/details/DetailsFragment.kt
+++ app/src/main/java/hu/bme/aut/android/kimiquotes/ui/details/DetailsFragment.kt
@@ -1,4 +1,4 @@
-package hu.bme.aut.android.kimiquotes.ui.dashboard
+package hu.bme.aut.android.kimiquotes.ui.details
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import hu.bme.aut.android.kimiquotes.databinding.FragmentDashboardBinding
+import hu.bme.aut.android.kimiquotes.databinding.FragmentNotificationsBinding
 
-class DashboardFragment : Fragment() {
+class DetailsFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentNotificationsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val notificationsViewModel =
+            ViewModelProvider(this).get(DetailsViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textNotifications
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/hu/bme/aut/android/kimiquotes/ui/newquote/NewQuoteFragment.kt
+++ app/src/main/java/hu/bme/aut/android/kimiquotes/ui/newquote/NewQuoteFragment.kt
@@ -1,4 +1,4 @@
-package hu.bme.aut.android.kimiquotes.ui.notifications
+package hu.bme.aut.android.kimiquotes.ui.newquote
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import hu.bme.aut.android.kimiquotes.databinding.FragmentNotificationsBinding
+import hu.bme.aut.android.kimiquotes.databinding.FragmentDashboardBinding
 
-class NotificationsFragment : Fragment() {
+class NewQuoteFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentDashboardBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val dashboardViewModel =
+            ViewModelProvider(this).get(NewQuoteViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
