9712dd61cae85f785629ba2ec3c14f37197d71fe
tothbece <tothbece@lauder.hu>
2022-05-19 17:22:39 +0200

    toast

--- app/src/main/java/hu/bme/aut/android/kimiquotes/ui/newquote/NewQuoteFragment.kt
+++ app/src/main/java/hu/bme/aut/android/kimiquotes/ui/newquote/NewQuoteFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import hu.bme.aut.android.kimiquotes.databinding.FragmentDashboardBinding
@@ -27,6 +28,10 @@ class NewQuoteFragment : Fragment() {
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        binding.button.setOnClickListener {
+            Toast.makeText(context, "Created", Toast.LENGTH_SHORT).show()
+        }
 
         return root
     }
